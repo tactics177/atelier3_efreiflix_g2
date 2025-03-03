020e37a0a4240765f617e07e10f638cd3ac8ef0e
pathi14 <pathipayong@gmail.com>
2025-03-03 16:38:11 +0100

    feat: clone du microfrontend

---
+++ catalogue_G1/src/Catalogue.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './styles.css';
+
+const Catalogue = () => {
+  return (
+    <div className="container mx-auto p-4">
+      <h1 className="text-2xl font-bold mb-4">Catalogue MFE</h1>
+      <div className="bg-white shadow-md rounded-lg p-6">
+        <p className="text-gray-700">
+          Catalogue test
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Catalogue; 
