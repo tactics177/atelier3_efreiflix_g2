be3074bc80ca942249a8bd927502c1ac2c7bfd1b
Paul-HenryN <ngpaulhenry@gmail.com>
2025-03-03 17:53:16 +0100

    feat: implement movie catalog with genre grouping and dynamic poster fetching

--- catalogue_G1/src/Catalogue.js
+++ catalogue_G1/src/Catalogue.js
@@ -1,16 +1,68 @@
-import React from 'react';
-import './styles.css';
-import Product from './components/Product';
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import { ProductsList } from "./components/ProductsList";
+import "./styles.css";
+
+function groupMoviesByCategory(movies) {
+  // Initialize an empty object to store movies by genre
+  const moviesByGenre = {};
+
+  // Loop through each movie
+  movies.forEach((movie) => {
+    // Loop through each genre of the movie
+    movie.genres.forEach((genre) => {
+      // If the genre doesn't exist as a key yet, create an empty array
+      if (!moviesByGenre[genre]) {
+        moviesByGenre[genre] = [];
+      }
+
+      // Add the movie to the array corresponding to the genre
+      moviesByGenre[genre].push({
+        id: movie.id,
+        title: movie.title,
+        year: movie.year,
+        description: movie.description,
+        posterUrl: movie.posterUrl,
+        trailerUrl: movie.trailerUrl,
+        rating: movie.rating,
+        genres: movie.genres,
+      });
+    });
+  });
+
+  return moviesByGenre;
+}
 
 const Catalogue = () => {
+  const [products, setProducts] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    setLoading(true);
+    axios
+      .get("http://localhost:3001/movies")
+      .then((response) => {
+        setProducts(response.data);
+        setLoading(false);
+      })
+      .catch((error) => {
+        console.error("Error fetching data: ", error);
+        setLoading(false);
+      });
+  }, []);
+
+  const moviesByGenre = groupMoviesByCategory(products);
+
   return (
-    <div className="container mx-auto p-4">
-      <h1 className="text-2xl font-bold mb-4">Catalogue MFE</h1>
-      <div className="bg-white shadow-md rounded-lg p-6">
-        <Product product={{name: "Product 1", image: "https://occ-0-56-55.1.nflxso.net/dnm/api/v6/Qs00mKCpRvrkl3HZAN5KwEL1kpE/AAAABS60wkIyJE-j7KLsjILo9JH9TWyJ4ZsVC5ZrJySxR_A4w3BQp4Ihccg4VTTzjctfOTTIzjXAty8qfY_gNTVM687gpFUVG9FEgSALaNrWldiGRHNuc2Ki_H0ukVvirydEoITJ.jpg?r=ea2"}} />
-      </div>
+    <div className="rounded-lg p-6">
+      {Object.entries(moviesByGenre).map(([genre, movies]) => (
+        <div key={genre}>
+          <h2 className="text-2xl font-semibold mb-2">{genre}</h2>
+          <ProductsList products={movies} />
+        </div>
+      ))}
     </div>
   );
 };
 
-export default Catalogue; 
+export default Catalogue;
